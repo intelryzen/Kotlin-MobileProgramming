38d6babc8924a0bd833b371ce65fbf6805599f57
우승식 <intelryzen@naver.com>
2025-03-29 15:38:42 +0900

    chore

--- bweek04b/src/main/java/com/example/bweek04b/model/ToDoItemFactory.kt
+++ bweek04b/src/main/java/com/example/bweek04b/model/ToDoItemFactory.kt
@@ -4,8 +4,8 @@ import androidx.compose.runtime.mutableStateListOf
 
 object ToDoItemFactory {
     fun makeToDoList() = mutableStateListOf(
-        Item("모프 공부하기1", "03-18 12:46"),
-        Item("모프 공부하기2", "03-18 13:46", ToDoStatus.COMPLETED),
-        Item("모프 공부하기3", "03-18 14:46", ToDoStatus.COMPLETED),
+        Item("201811200 우승식 1", "03-18 12:46"),
+        Item("201811200 우승식 2", "03-18 13:46", ToDoStatus.COMPLETED),
+        Item("201811200 우승식 3", "03-18 14:46", ToDoStatus.COMPLETED),
     )
 }
