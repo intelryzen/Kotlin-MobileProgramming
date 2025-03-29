169a0741ccd895f4a9329dcc839bacd3d07c5201
우승식 <intelryzen@naver.com>
2025-03-29 14:09:20 +0900

    style: 버튼 정렬

--- bweek04b/src/main/java/com/example/bweek04b/uicomponents/ToDoItemInput.kt
+++ bweek04b/src/main/java/com/example/bweek04b/uicomponents/ToDoItemInput.kt
@@ -2,6 +2,7 @@ package com.example.bweek04b.uicomponents
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -10,8 +11,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.bweek04b.model.Item
 import com.example.bweek04b.model.ToDoItemFactory
 import java.time.LocalDateTime
@@ -21,7 +24,10 @@ import java.time.format.DateTimeFormatter
 @Composable
 fun ToDoItemInput(toDoList: MutableList<Item>) {
     var textFieldState by remember { mutableStateOf("") }
-    Row(modifier = Modifier.fillMaxWidth()) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         TextField(
             modifier = Modifier.weight(1f),
             value = textFieldState,
@@ -32,7 +38,9 @@ fun ToDoItemInput(toDoList: MutableList<Item>) {
                 Text("할 일을 입력하세요.")
             }
         )
-        Button(onClick = {
+        Button(
+            modifier = Modifier.padding(horizontal = 10.dp),
+            onClick = {
             toDoList.add(
                 Item(
                     textFieldState,
