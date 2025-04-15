1bb051e3c36218ee643a3b6cde4202a862b27f00
우승식 <intelryzen@naver.com>
2025-04-15 16:09:38 +0900

    feat: bweek07a 화면전환

---
+++ bweek07a/src/main/java/com/example/bweek07a/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.bweek07a
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bweek07a.example01.uicomponents.MainScreen
+import com.example.bweek07a.ui.theme.BTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BTheme {
+                MainScreen()
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BTheme {
+        Greeting("Android")
+    }
+}
---
+++ bweek07a/src/main/java/com/example/bweek07a/example01/model/Routes.kt
@@ -0,0 +1,9 @@
+package com.example.bweek07a.example01.model
+
+sealed class Routes(val route: String) {
+    object Home : Routes(route = "Home")
+    object ScreenA : Routes(route = "A")
+    object ScreenB : Routes(route = "B")
+    object ScreenC : Routes(route = "C")
+    object ScreenD : Routes(route = "D")
+}
