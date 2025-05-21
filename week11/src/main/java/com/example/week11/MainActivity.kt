ae92021196815d7d16d306cb4c0f38d7b261ebb0
우승식 <intelryzen@naver.com>
2025-05-21 10:51:36 +0900

    feat: 다음 뉴스 fetch

---
+++ week11/src/main/java/com/example/week11/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.week11
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.week11.example01.FetchDaumNews
+import com.example.week11.ui.theme.BTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BTheme {
+                FetchDaumNews()
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BTheme {
+        Greeting("Android")
+    }
+}
